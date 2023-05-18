2939de13e134f6e93d7e8edc5bff3d5a921ae279
Kelli Libin <km.putnam2@gmail.com>
2023-05-18 16:48:11 -0600

    made separate errors, index to import/export

---
+++ errors/index.js
@@ -0,0 +1,7 @@
+const CustomAPIError = require('./custom-error')
+const BadRequest = require('./bad-request')
+const UnauthenticatedError = require('./unauthenticated')
+
+module.exports = {
+    CustomAPIError, BadRequest, UnauthenticatedError
+}
